585d3d4d04fd301561d7ca0d3d73230b6b6b0c37
gudzan <andreeva.anva@gmail.com>
2024-10-29 14:07:09 +0300

    test8

--- vk-test-app/src/api/repositories.ts
+++ vk-test-app/src/api/repositories.ts
@@ -2,10 +2,9 @@ import axios from 'axios'
 import Sort from '../types/sort';
 
 const getRepositories = (sort: Sort, currentPage: number) => {
-  const encryptedText = "Z2hwX3NzMUNqN1c4S05CR3pEWDN6UXhFbUZtOXF1SWxBTDFxeVVBWQ==" 
-  const token = atob(encryptedText);
+  const token = "o1QPsZPH0o7oyZFH1jYsPkF3wYUBaD316jex"
   
-  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  axios.defaults.headers.common['Authorization'] = `Bearer ${"ghp_"+token}`;
   return axios.get(`https://api.github.com/search/repositories?q=javascript&sort=${sort.sortField}&order=${sort.sortOrder}&page=${currentPage}&per_page=10`)
 }
 
