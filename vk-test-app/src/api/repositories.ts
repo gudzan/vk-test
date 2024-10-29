6166f8c4eab08b2116ad294bf1172613b2d0411c
gudzan <andreeva.anva@gmail.com>
2024-10-29 12:50:31 +0300

    test2

--- vk-test-app/src/api/repositories.ts
+++ vk-test-app/src/api/repositories.ts
@@ -2,9 +2,7 @@ import axios from 'axios'
 import Sort from '../types/sort';
 
 const getRepositories = (sort: Sort, currentPage: number) => {
-  const token = import.meta.env.VITE_APP_API_KEY
-  console.log(token);
-  
+  const token = import.meta.env.VITE_APP_API_KEY  
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   return axios.get(`https://api.github.com/search/repositories?q=javascript&sort=${sort.sortField}&order=${sort.sortOrder}&page=${currentPage}&per_page=10`)
 }
