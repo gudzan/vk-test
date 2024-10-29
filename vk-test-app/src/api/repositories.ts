06030cb02447ce69f7a9fa6187f2bba658727057
gudzan <andreeva.anva@gmail.com>
2024-10-29 14:05:05 +0300

    test7

--- vk-test-app/src/api/repositories.ts
+++ vk-test-app/src/api/repositories.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 import Sort from '../types/sort';
 
 const getRepositories = (sort: Sort, currentPage: number) => {
-  const token = import.meta.env.VITE_APP_API_KEY  
-  console.log("t",token);
+  const encryptedText = "Z2hwX3NzMUNqN1c4S05CR3pEWDN6UXhFbUZtOXF1SWxBTDFxeVVBWQ==" 
+  const token = atob(encryptedText);
   
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   return axios.get(`https://api.github.com/search/repositories?q=javascript&sort=${sort.sortField}&order=${sort.sortOrder}&page=${currentPage}&per_page=10`)
