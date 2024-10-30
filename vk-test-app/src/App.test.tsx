53ceadf5b9910c011e13f83aa52bb662faac5ad1
gudzan <andreeva.anva@gmail.com>
2024-10-30 16:56:57 +0300

    done6

--- vk-test-app/src/App.test.tsx
+++ vk-test-app/src/App.test.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { render, screen } from "@testing-library/react"
-import '@testing-library/jest-dom';
-import App from "./App.tsx"
-import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
-import { SortProvider } from './hooks/useSort.tsx';
+// import React from 'react';
+// import { render } from "@testing-library/react"
+// import '@testing-library/jest-dom';
+// import App from "./App.tsx"
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store.ts";
+// import { SortProvider } from './hooks/useSort.tsx';
 
-test("есть ли элемент", () => {
-  const { container } = render(
-    <Provider store={store}>
-      <SortProvider>
-        <App />
-      </SortProvider>
-    </Provider>
-  )
+// test("есть ли элемент", () => {
+//   const { container } = render(
+//     <Provider store={store}>
+//       <SortProvider>
+//         <App />
+//       </SortProvider>
+//     </Provider>
+//   )
 
-  // const test = container.children
-  // expect(test).toBeInTheDocument()
-})
+//   // const test = container.children
+//   // expect(test).toBeInTheDocument()
+// })
